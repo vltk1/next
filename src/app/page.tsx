149a04472aac8bc7cf2148cc990f3d306e5cb7b5
nextpage <duocifv@gmail.com>
2023-11-05 18:20:15 +0700

    Up next

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='mx-auto text-center'>
+      <h1 className='text-xl'>Hello Word</h1>
+    </main>
+  )
+}
