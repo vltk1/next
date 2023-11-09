a8597c63c9c77ff4a44e9764cfe2c2b244bad5b8
nextpage <duocifv@gmail.com>
2023-11-09 21:15:19 +0700

    folder

---
+++ hotels/src/constants/index.ts
@@ -0,0 +1,19 @@
+export const exploreWorlds = [
+    {
+        id: 'wodl-1',
+        imgUrl: 'sdf.png',
+        title: 'The hog'
+    },
+    {
+        id: 'wodl-1',
+        imgUrl: 'sdf.png',
+        title: 'The hog'
+    }
+]
+
+export const socails = [
+    {
+        name: 'twitter',
+        url: '/twitter.svg'
+    }
+]
